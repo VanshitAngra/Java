623a186a5ea44201397cb938eba22b6aefc844dc
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-04-25 14:53:51 +0530

    6th commit

---
+++ arr/MaximumSumPathinTwoArrays.java
@@ -0,0 +1,41 @@
+package arr;
+
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			int[] arr = new int[n];
+			int[] brr = new int[m];
+			for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+			for(int j=0;j<m;j++) brr[j] = sc.nextInt();
+			int res = 0;
+			int s1 = 0;
+			int s2 = 0;
+			int i = 0;
+			int j = 0;
+			while(i<n && j<m){
+				if(arr[i]<brr[j]){
+					s1+=arr[i++];
+				}
+				else if(arr[i]>brr[j]){
+					s2+=brr[j++];
+				}
+				else{
+					res+=Math.max(s1,s2) + arr[i];
+					s1 = 0;
+					s2 = 0;
+					i++;
+					j++;
+				}
+			}
+			while(i<n) s1+=arr[i++];
+			while(j<m) s2+=brr[j++];
+			res+=Math.max(s1,s2);
+			System.out.println(res);
+		}
+    }
+}
--- str/strin.java
+++ str/strin.java
@@ -3,14 +3,22 @@ import java.util.*;
 
 public class strin {
     public static void main(String[] args) {
-        String s="abc";
+        String s="Hello";
         String s1="abc";
         String s2= new String("abf");
-        System.out.println(s==s1);
-        System.out.println(s==s2);
-        System.out.println(s1.equals(s2));
-        System.out.println(s1.length());
-        System.out.println(s1.charAt(2));
-        System.out.println(s1.compareTo(s2));        
+        // System.out.println(s==s1);
+        // System.out.println(s==s2);
+        // System.out.println(s1.equals(s2));
+        // System.out.println(s1.length());
+        // System.out.println(s1.charAt(2));
+        // System.out.println(s1.compareTo(s2));
+        // System.out.println(s.substring(1));        
+        // System.out.println(s.substring(2));    
+        // System.out.println(s.substring(0,s.length()-1));    
+        for(int i=0;i<s.length();i++)
+        {
+            for(int j=i;j<s.length();j++)
+            System.out.println(s.substring(i,j+1));
+        }
     }
 }
