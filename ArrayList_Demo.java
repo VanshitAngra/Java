e3c08b95feed9b9d58e877da6a58b5e24452df77
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-05-23 13:48:06 +0530

    15 th commit addition of ArrayList_Demo.class 	ArrayList_Demo.java

---
+++ ArrayList_Demo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class ArrayList_Demo {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+        list.add(1, 100);
+        System.out.println(list);
+        for(ArrayList k : list)
+        {
+            System.out.println(k);
+        }
+    }
+}
