95f22a4ae8e2a1f083eae0c2498745a474ed1735
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2023-05-26 11:41:53 +0530

    addition of groupanagram

---
+++ arr/longestConsecutive.java
@@ -0,0 +1,34 @@
+package arr;
+import java.util.HashMap;
+//128 leet code
+public class longestConsecutive {
+    public static void main(String[] args) {
+        int arr[]={4,3,2,1};
+        HashMap<Integer,Boolean> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i]-1)){
+                map.put(arr[i],false);
+            }
+            else{
+                map.put(arr[i],true);
+            }
+            if(map.containsKey(arr[i]+1)){
+                map.put(arr[i]+1,false);
+            }
+        }
+        System.out.println(map);
+        int ans=0;
+        for(int a:map.keySet()){
+            if(map.get(a)){
+                int count=0;
+                int key=a;
+                while(map.containsKey(key)){
+                    count++;
+                    key++;
+                }
+                ans=Math.max(count,ans);
+            }
+        }
+        System.out.println(ans);
+    }
+}
---
+++ str/GroupAnagram.java
@@ -0,0 +1,7 @@
+package str;
+import java.util.*;
+public class GroupAnagram {
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+    }
+}
